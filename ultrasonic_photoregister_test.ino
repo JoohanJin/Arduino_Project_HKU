282aae11520e60b0bacd0e51c460842a79b2bd1a
Joe <61616324+JoohanJin@users.noreply.github.com>
2024-04-20 20:11:52 +0800

    feat: add chapter 1- required function
    The list of file:
    1. ultrasonic_photoregister_test.ino: ultrasonic and photo-register sensor test code
    2. vehicle_auto_move_test.ino: automated movement of the vehicle
    3. required_func.ino: the combined code for the required function
    4. gyro_test.ino: gyro sensor calibration and test code.

---
+++ ultrasonic_photoregister_test.ino
@@ -0,0 +1,125 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <MPU6050_light.h>
+
+
+// need to initalize each element for ultrasonic and photo-register
+// Ultrasonic
+// need to figure out the pin #
+#define echoPin 11
+#define trigPin 12
+
+long distance_in_cm;
+
+
+// photo register
+//variables for light intensity to ADC reading equations 
+int int_adc0, int_adc0_c, int_adc0_m; // from the left
+int int_adc1, int_adc1_c, int_adc1_m; // from the right
+int int_left, int_right;
+
+
+// Gyro
+MPU6050 mpu(Wire);
+unsigned long timer = 0;
+
+
+void setup_ultrasonic(){
+  // Ultrasonic
+  pinMode(echoPin, INPUT);
+  pinMode(trigPin, OUTPUT);
+}
+
+
+void setup_photoregister(){
+  // Photoregister
+  // measure the sensors reading at ambient light intensity  
+  Serial.println("Calibration in progress, put the sensor under the light (~ 5 sec) ......");
+  Serial.println("***********************");
+  delay(2000);        // delay 5000 ms
+
+  int_adc0 = analogRead(A0);   // sensor at ambient light intensity
+  int_adc1 = analogRead(A1);
+  Serial.print("Left : ");
+  Serial.println(int_adc0);
+  Serial.println(int_adc1);
+  delay(1000); 
+
+  Serial.println("\nCalibration in progress, cover the sensor with your fingers (~ 8 sec to set)......");
+  Serial.println("************ Put Fingers *****************");
+  delay(2000);        // delay 5000 ms
+  Serial.println("********* START Calibration **************");
+
+  // measure the sensors reading at zero light intensity  
+  int_adc0_c=analogRead(A0);   // Left sensor at zero light intensity
+  int_adc1_c=analogRead(A1);   // Left sensor at zero light intensity
+
+  // calculate the slope of light intensity to ADC reading equations  
+  int_adc0_m=(int_adc0 - int_adc0_c)/100;
+  int_adc1_m=(int_adc1 - int_adc1_c)/100;
+  delay(3000);        // delay 10000 ms 
+   
+  Serial.println("\n******** Completed! Remove your hands ********");
+}
+
+void setup_gyro(){
+  Wire.begin();
+  mpu.begin();
+  Serial.println("Calculating gyro offset, do not move MPU6050");
+  Serial.println("............");
+  delay(1000);
+  mpu.calcGyroOffsets();                          // This does the calibration
+  Serial.println("Done"); 
+}
+
+void setup(){
+  Serial.begin(115200);
+  setup_photoregister();
+}
+
+void measure_distance(){
+  long duration;
+
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+  distance_in_cm = (duration/2.0) / 29.1;
+}
+
+void measure_light_intensity(){
+  int_left=(analogRead(A0)-int_adc0_c)/int_adc0_m;
+  int_right=(analogRead(A1)-int_adc1_c)/int_adc1_m;
+
+  Serial.print("from left: ");
+  Serial.println(int_right);
+  Serial.print("from right: ");
+  Serial.println(int_left);
+}
+
+
+void gyro(){
+  mpu.update();  
+  if((millis()-timer)>10)                         // print data every 10ms
+  {                                             
+    Serial.print("Pitch (Angle_X) : ");
+    Serial.print(mpu.getAngleX());
+    Serial.print("  Roll  (Angle_Y) : ");
+    Serial.print(mpu.getAngleY());
+    Serial.print("  Yaw   (Angle_Z) : ");
+    Serial.println(mpu.getAngleZ());
+    //Serial.println("==========================");
+    timer = millis();  
+    //delay(1000);
+  }
+}
+
+void loop(){
+  measure_light_intensity();
+}
---
+++ vehicle_auto_move_test.ino
@@ -0,0 +1,471 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <MPU6050_light.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+
+// intialize the gyro
+MPU6050 mpu(Wire);
+unsigned long timer = 0;
+
+// need to initalize each element for ultrasonic and light intensity sensor
+
+
+
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     28 //4 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+int oldV = 1, newV = 0;
+#include <SoftwareSerial.h>
+//UNO: (2, 3)
+//SoftwareSerial mySerial(4, 6); // RX, TX
+int pan = 90;
+int tilt = 120;
+int window_size = 0;
+int BT_alive_cnt = 0;
+int voltCount = 0;
+#include <Servo.h>
+Servo servo_pan;
+Servo servo_tilt;
+int servo_min = 20;
+int servo_max = 160;
+
+unsigned long time;
+
+//FaBoPWM faboPWM;
+int pos = 0;
+int MAX_VALUE = 2000;
+int MIN_VALUE = 300;
+
+// Define motor pins
+#define PWMA 12    //Motor A PWM
+#define DIRA1 34
+#define DIRA2 35  //Motor A Direction
+#define PWMB 8    //Motor B PWM
+#define DIRB1 37
+#define DIRB2 36  //Motor B Direction
+#define PWMC 6   //Motor C PWM
+#define DIRC1 43
+#define DIRC2 42  //Motor C Direction
+#define PWMD 5    //Motor D PWM
+#define DIRD1 A4  //26  
+#define DIRD2 A5  //27  //Motor D Direction
+
+#define MOTORA_FORWARD(pwm)    do{digitalWrite(DIRA1,LOW); digitalWrite(DIRA2,HIGH);analogWrite(PWMA,pwm);}while(0)
+#define MOTORA_STOP(x)         do{digitalWrite(DIRA1,LOW); digitalWrite(DIRA2,LOW); analogWrite(PWMA,0);}while(0)
+#define MOTORA_BACKOFF(pwm)    do{digitalWrite(DIRA1,HIGH);digitalWrite(DIRA2,LOW); analogWrite(PWMA,pwm);}while(0)
+
+#define MOTORB_FORWARD(pwm)    do{digitalWrite(DIRB1,LOW); digitalWrite(DIRB2,HIGH);analogWrite(PWMB,pwm);}while(0)
+#define MOTORB_STOP(x)         do{digitalWrite(DIRB1,LOW); digitalWrite(DIRB2,LOW); analogWrite(PWMB,0);}while(0)
+#define MOTORB_BACKOFF(pwm)    do{digitalWrite(DIRB1,HIGH);digitalWrite(DIRB2,LOW); analogWrite(PWMB,pwm);}while(0)
+
+#define MOTORC_FORWARD(pwm)    do{digitalWrite(DIRC1,LOW); digitalWrite(DIRC2,HIGH);analogWrite(PWMC,pwm);}while(0)
+#define MOTORC_STOP(x)         do{digitalWrite(DIRC1,LOW); digitalWrite(DIRC2,LOW); analogWrite(PWMC,0);}while(0)
+#define MOTORC_BACKOFF(pwm)    do{digitalWrite(DIRC1,HIGH);digitalWrite(DIRC2,LOW); analogWrite(PWMC,pwm);}while(0)
+
+#define MOTORD_FORWARD(pwm)    do{digitalWrite(DIRD1,LOW); digitalWrite(DIRD2,HIGH);analogWrite(PWMD,pwm);}while(0)
+#define MOTORD_STOP(x)         do{digitalWrite(DIRD1,LOW); digitalWrite(DIRD2,LOW); analogWrite(PWMD,0);}while(0)
+#define MOTORD_BACKOFF(pwm)    do{digitalWrite(DIRD1,HIGH);digitalWrite(DIRD2,LOW); analogWrite(PWMD,pwm);}while(0)
+
+#define SERIAL  Serial
+#define BTSERIAL Serial3
+
+#define LOG_DEBUG
+
+#ifdef LOG_DEBUG
+#define M_LOG SERIAL.print
+#else
+#define M_LOG BTSERIAL.println
+#endif
+
+//PWM Definition
+#define MAX_PWM   2000
+#define MIN_PWM   300
+
+int Motor_PWM = 300;
+
+
+//    ↑A-----B↑
+//     |  ↑  |
+//     |  |  |
+//    ↑C-----D↑
+void BACK(uint8_t pwm_A, uint8_t pwm_B, uint8_t pwm_C, uint8_t pwm_D)
+{
+  MOTORA_BACKOFF(Motor_PWM);
+  MOTORB_FORWARD(Motor_PWM);
+  MOTORC_BACKOFF(Motor_PWM);
+  MOTORD_FORWARD(Motor_PWM);
+}
+
+//    ↓A-----B↓
+//     |  |  |
+//     |  ↓  |
+//    ↓C-----D↓
+void ADVANCE()
+{
+  MOTORA_FORWARD(Motor_PWM);
+  MOTORB_BACKOFF(Motor_PWM);
+  MOTORC_FORWARD(Motor_PWM);
+  MOTORD_BACKOFF(Motor_PWM);
+}
+//    =A-----B↑
+//     |   ↖ |
+//     | ↖   |
+//    ↑C-----D=
+void LEFT_1()
+{
+  MOTORA_STOP(Motor_PWM);
+  MOTORB_FORWARD(Motor_PWM);
+  MOTORC_BACKOFF(Motor_PWM);
+  MOTORD_STOP(Motor_PWM);
+}
+
+//    ↓A-----B↑
+//     |  ←  |
+//     |  ←  |
+//    ↑C-----D↓
+void RIGHT_2()
+{
+  MOTORA_FORWARD(Motor_PWM);
+  MOTORB_FORWARD(Motor_PWM);
+  MOTORC_BACKOFF(Motor_PWM);
+  MOTORD_BACKOFF(Motor_PWM);
+}
+//    ↓A-----B=
+//     | ↙   |
+//     |   ↙ |
+//    =C-----D↓
+void LEFT_3()
+{
+  MOTORA_FORWARD(Motor_PWM);
+  MOTORB_STOP(Motor_PWM);
+  MOTORC_STOP(Motor_PWM);
+  MOTORD_BACKOFF(Motor_PWM);
+}
+//    ↑A-----B=
+//     | ↗   |
+//     |   ↗ |
+//    =C-----D↑
+void RIGHT_1()
+{
+  MOTORA_BACKOFF(Motor_PWM);
+  MOTORB_STOP(Motor_PWM);
+  MOTORC_STOP(Motor_PWM);
+  MOTORD_FORWARD(Motor_PWM);
+}
+//    ↑A-----B↓
+//     |  →  |
+//     |  →  |
+//    ↓C-----D↑
+void LEFT_2()
+{
+  MOTORA_BACKOFF(Motor_PWM);
+  MOTORB_BACKOFF(Motor_PWM);
+  MOTORC_FORWARD(Motor_PWM);
+  MOTORD_FORWARD(Motor_PWM);
+}
+//    =A-----B↓
+//     |   ↘ |
+//     | ↘   |
+//    ↓C-----D=
+void RIGHT_3()
+{
+  MOTORA_STOP(Motor_PWM);
+  MOTORB_BACKOFF(Motor_PWM);
+  MOTORC_FORWARD(Motor_PWM);
+  MOTORD_STOP(Motor_PWM);
+}
+
+//    ↑A-----B↓
+//     | ↗ ↘ |
+//     | ↖ ↙ |
+//    ↑C-----D↓
+
+void rotate_1()  //tate_1(uint8_t pwm_A,uint8_t pwm_B,uint8_t pwm_C,uint8_t pwm_D)
+{
+  MOTORA_BACKOFF(300);
+  MOTORB_BACKOFF(300);
+  MOTORC_BACKOFF(300);
+  MOTORD_BACKOFF(300);
+}
+
+//    ↓A-----B↑
+//     | ↙ ↖ |
+//     | ↘ ↗ |
+//    ↓C-----D↑
+
+void rotate_2()  // rotate_2(uint8_t pwm_A,uint8_t pwm_B,uint8_t pwm_C,uint8_t pwm_D)
+{
+  MOTORA_FORWARD(300);
+  MOTORB_FORWARD(300);
+  MOTORC_FORWARD(300);
+  MOTORD_FORWARD(300);
+}
+
+//    =A-----B=
+//     |  =  |
+//     |  =  |
+//    =C-----D=
+
+void STOP()
+{
+  MOTORA_STOP(Motor_PWM);
+  MOTORB_STOP(Motor_PWM);
+  MOTORC_STOP(Motor_PWM);
+  MOTORD_STOP(Motor_PWM);
+}
+
+void UART_Control()
+{
+  String myString;
+  char BT_Data = 0;
+  // USB data
+  /****
+     Check if USB Serial data contain brackets
+  */
+
+  if (SERIAL.available())
+  {
+    char inputChar = SERIAL.read();
+    if (inputChar == '(') { // Start loop when left bracket detected
+      myString = "";
+      inputChar = SERIAL.read();
+      while (inputChar != ')')
+      {
+        myString = myString + inputChar;
+        inputChar = SERIAL.read();
+        if (!SERIAL.available()) {
+          break;
+        }// Break when bracket closed
+      }
+    }
+    int commaIndex = myString.indexOf(','); //Split data in bracket (a, b, c)
+    //Search for the next comma just after the first
+    int secondCommaIndex = myString.indexOf(',', commaIndex + 1);
+    String firstValue = myString.substring(0, commaIndex);
+    String secondValue = myString.substring(commaIndex + 1, secondCommaIndex);
+    String thirdValue = myString.substring(secondCommaIndex + 1); // To the end of the string
+    if ((firstValue.toInt() > servo_min and firstValue.toInt() < servo_max) and  //Convert them to numbers
+        (secondValue.toInt() > servo_min and secondValue.toInt() < servo_max)) {
+      pan = firstValue.toInt();
+      tilt = secondValue.toInt();
+      window_size = thirdValue.toInt();
+    }
+    SERIAL.flush();
+    Serial3.println(myString);
+    Serial3.println("Done");
+    if (myString != "") {
+      display.clearDisplay();
+      display.setCursor(0, 0);     // Start at top-left corner
+      display.println("Serial_Data = ");
+      display.println(myString);
+      display.display();
+    }
+  }
+
+  //BT Control
+  /*
+    Receive data from app and translate it to motor movements
+  */
+  // BT Module on Serial 3 (D14 & D15)
+  if (Serial3.available())
+  {
+    BT_Data = Serial3.read();
+    SERIAL.print(BT_Data);
+    Serial3.flush();
+    BT_alive_cnt = 100;
+    display.clearDisplay();
+    display.setCursor(0, 0);     // Start at top-left corner
+    display.println("BT_Data = ");
+    display.println(BT_Data);
+    display.display();
+  }
+
+  BT_alive_cnt = BT_alive_cnt - 1;
+  if (BT_alive_cnt <= 0) {
+    STOP();
+  }
+  switch (BT_Data)
+  {
+    case 'A':  ADVANCE();  M_LOG("Run!\r\n"); break;
+    case 'B':  RIGHT_2();  M_LOG("Right up!\r\n");     break;
+    case 'C':  rotate_1();                            break;
+    case 'D':  RIGHT_3();  M_LOG("Right down!\r\n");   break;
+    case 'E':  BACK(500, 500, 500, 500);     M_LOG("Run!\r\n");          break;
+    case 'F':  LEFT_3();   M_LOG("Left down!\r\n");    break;
+    case 'G':  rotate_2();                              break;
+    case 'H':  LEFT_2();   M_LOG("Left up!\r\n");     break;
+    case 'Z':  STOP();     M_LOG("Stop!\r\n");        break;
+    case 'z':  STOP();     M_LOG("Stop!\r\n");        break;
+    case 'd':  LEFT_2();   M_LOG("Left!\r\n");        break;
+    case 'b':  RIGHT_2();  M_LOG("Right!\r\n");        break;
+    case 'L':  Motor_PWM = 1500;                      break;
+    case 'M':  Motor_PWM = 500;                       break;
+  }
+}
+
+/*Voltage Readings transmitter
+  Sends them via Serial3*/
+void sendVolt() {
+  newV = analogRead(A0);
+  if (newV != oldV) {
+    if (!Serial3.available()) {
+      Serial3.println(newV);
+      Serial.println(newV);
+    }
+  }
+  oldV = newV;
+}
+
+
+void setup(){
+  SERIAL.begin(115200);
+  Wire.begin();
+  mpu.begin();
+
+  //OLED SETUP
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
+    Serial.println(F("SSD1306 allocation failed"));
+  }
+  display.clearDisplay();
+  display.setTextSize(2);      // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE); // Draw white text
+  display.cp437(true);         // Use full 256 char 'Code Page 437' font
+  display.setCursor(0, 0);     // Start at top-left corner
+  display.println("AI Robot");
+  display.display();
+  
+  Serial.println("Calculating gyro offset, do not move MPU6050");
+  Serial.println(".............");
+  delay(1000);
+  mpu.calcGyroOffsets();
+  SERIAL.println("Done Calibration");
+
+  // STOP THE ROBOT
+  STOP();
+
+  // BT serial setup
+  Serial3.begin(9600); 
+
+  //Pan=PL4=>48, Tilt=PL5=>47
+  servo_pan.attach(48);
+  servo_tilt.attach(47);
+
+  
+
+
+  //Setup Voltage detector
+  pinMode(A0, INPUT);
+}
+
+void loop(){
+  double startAngle;
+  double currentAngle;
+  // need to figure out
+  double rotate_90_time;
+  rotate_90_time = 1250;
+
+  // move forward
+  Serial.println("move forward");
+  display.clearDisplay();display.setCursor(0, 0);
+  display.println("Forward");
+  display.display();
+  ADVANCE();
+  delay(2000);
+  Serial.println("=================================done=================================");
+  STOP();
+
+
+  // move backward
+  Serial.println("move backward");
+  // need to figure out the optimum value, while it is not used in the function.
+  display.clearDisplay();display.setCursor(0, 0);
+  display.println("Backward");
+  display.display();
+  BACK(500, 500, 500, 500);
+  delay(2000);
+  Serial.println("=================================done=================================");
+  STOP();
+
+
+  // CW rotation 90 degree
+  Serial.println("90 degree clockwise");
+  display.clearDisplay();display.setCursor(0, 0);
+  display.println("CW 90 degree");
+  display.display();
+  rotate_2();
+  delay(rotate_90_time);
+  Serial.println("=================================done=================================");
+  STOP();
+  delay(2000);
+
+
+
+  // CCW rotation 90 degree
+  Serial.println("90 degree counter-clockwise");
+  display.clearDisplay();display.setCursor(0, 0);
+  display.println("CCW 90 degree");
+  display.display();
+  rotate_1();
+  delay(rotate_90_time);
+  Serial.println("=================================done=================================");
+  STOP();
+  delay(2000);
+
+  Serial.println("=================================done=================================");
+  STOP();
+  delay(2000);
+
+
+  // CW rotation 360 degree
+  Serial.println("360 degree clockwise");
+  display.clearDisplay();display.setCursor(0, 0);
+  display.println("CW 360 degree");
+  display.display();
+  rotate_2();
+  delay(rotate_90_time * 4);
+  Serial.println("=================================done=================================");
+  STOP();
+  delay(2000);
+
+
+  // CW rotation 180 degree
+  Serial.println("180 degree counter-clockwise");
+  display.clearDisplay();display.setCursor(0, 0);
+  display.println("CW 180 degree");
+  display.display();
+  rotate_2();
+  delay(rotate_90_time * 2);
+  Serial.println("=================================done=================================");
+  STOP();
+  delay(2000);
+
+
+  // CCW rotation 360 degree
+  Serial.println("360 degree counter-clockwise");
+  display.clearDisplay();display.setCursor(0, 0);
+  display.println("CCW 360 degree");
+  display.display();
+  rotate_1();
+  delay(rotate_90_time * 4);
+  Serial.println("=================================done=================================");
+  STOP();
+  delay(2000);
+
+
+  // CCW rotation 180 degree
+  Serial.println("180 degree counter-clockwise");
+  display.clearDisplay();display.setCursor(0, 0);
+  display.println("CCW 180 degree");
+  display.display();
+  rotate_1();
+  delay(rotate_90_time * 2);
+  Serial.println("=================================done=================================");
+  STOP();
+  delay(2000);
+}
